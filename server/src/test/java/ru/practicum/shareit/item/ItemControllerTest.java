8c0896f31e029a6ecef85a495af96b036870837d
pak sem <p4ck.sem@yandex.ru>
2023-10-02 20:40:52 +0300

    refactor: вернул логирование, убрал валидацию из server, убрал из server тесты на валидацию

--- server/src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.practicum.shareit.exception.NotFoundException;
-import ru.practicum.shareit.exception.ValidationException;
 import ru.practicum.shareit.item.comment.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
