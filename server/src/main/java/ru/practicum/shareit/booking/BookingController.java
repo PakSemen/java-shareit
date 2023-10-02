fec4d985ac5f3a0154977bdd25a46e244dff9fa8
pak sem <p4ck.sem@yandex.ru>
2023-10-02 19:40:34 +0300

    refactor: вернул логирование, убрал валидацию из server

--- server/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ server/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.booking;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.BookingDto;
@@ -15,43 +16,48 @@ import static ru.practicum.shareit.util.Constants.REQUEST_HEADER_USER_ID;
 @RestController
 @RequestMapping(path = "/bookings")
 @RequiredArgsConstructor
-@Validated
+@Slf4j
 public class BookingController {
     private final BookingService bookingService;
 
     @PostMapping()
-    public BookingDto addBooking(@RequestHeader(REQUEST_HEADER_USER_ID) Long id,
+    public BookingDto addBooking(@RequestHeader(REQUEST_HEADER_USER_ID) Long userId,
                                  @RequestBody BookingShortDto bookingShortDto) {
-        return bookingService.createBooking(id, bookingShortDto);
+        log.info("Add booking: {} for user with id = {}",bookingShortDto, userId);
+        return bookingService.createBooking(userId, bookingShortDto);
     }
 
     @GetMapping("/{bookingId}")
-    public BookingDto getBookingById(@RequestHeader(REQUEST_HEADER_USER_ID) Long id,
+    public BookingDto getBookingById(@RequestHeader(REQUEST_HEADER_USER_ID) Long userId,
                                      @PathVariable Long bookingId) {
-        return bookingService.getBookingById(id, bookingId);
+        log.info("Get booking with id = {} from user with id = {}", bookingId, userId);
+        return bookingService.getBookingById(userId, bookingId);
     }
 
     @GetMapping()
-    public List<BookingDto> getAllBookingByState(@RequestHeader("X-Sharer-User-Id") Long id,
+    public List<BookingDto> getAllBookingByState(@RequestHeader("X-Sharer-User-Id") Long userId,
                                                  @RequestParam(defaultValue = "ALL") String state,
-                                                 @PositiveOrZero @RequestParam(defaultValue = "0") Integer from,
-                                                 @Positive @RequestParam(defaultValue = "10") Integer size) {
-        return bookingService.getAllBookingByState(id, state, from, size);
+                                                 @RequestParam(defaultValue = "0") Integer from,
+                                                 @RequestParam(defaultValue = "10") Integer size) {
+        log.info("Get all booking with state = {} for user with id = {}",state, userId);
+        return bookingService.getAllBookingByState(userId, state, from, size);
     }
 
     @GetMapping("/owner")
     public List<BookingDto> getAllItemsBookings(@RequestHeader("X-Sharer-User-Id") Long id,
                                                 @RequestParam(defaultValue = "ALL") String state,
-                                                @PositiveOrZero @RequestParam(defaultValue = "0") Integer from,
-                                                @Positive @RequestParam(defaultValue = "10") Integer size) {
+                                                @RequestParam(defaultValue = "0") Integer from,
+                                                @RequestParam(defaultValue = "10") Integer size) {
+        log.info("Get all owners booking with states = {}",state);
         return bookingService.getAllOwnersBookingByState(id, state, from, size);
     }
 
     @PatchMapping("/{bookingId}")
-    public BookingDto approveBooking(@RequestHeader(REQUEST_HEADER_USER_ID) Long id,
+    public BookingDto approveBooking(@RequestHeader(REQUEST_HEADER_USER_ID) Long userId,
                                      @PathVariable Long bookingId,
                                      @RequestParam Boolean approved) {
-        return bookingService.approveBooking(id, bookingId, approved);
+        log.info("Approve booking id = {} for user with id = {} is {}",bookingId, userId, approved);
+        return bookingService.approveBooking(userId, bookingId, approved);
     }
 
 }
--- server/src/main/java/ru/practicum/shareit/item/ItemController.java
+++ server/src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.item;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.comment.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
@@ -14,23 +15,27 @@ import static ru.practicum.shareit.util.Constants.REQUEST_HEADER_USER_ID;
 @RestController
 @RequestMapping("/items")
 @RequiredArgsConstructor
+@Slf4j
 public class ItemController {
     private final ItemService service;
 
     @PostMapping
-    public ItemDto createItem(@Valid @RequestBody ItemShortDto itemShortDto,
+    public ItemDto createItem(@RequestBody ItemShortDto itemShortDto,
                               @RequestHeader(REQUEST_HEADER_USER_ID) Long userId) {
+        log.info("The item with id = {} has been created", itemShortDto.getId());
         return service.createItem(itemShortDto, userId);
     }
 
     @GetMapping("/{itemId}")
     public ItemDto getItemById(@PathVariable Long itemId,
                                @RequestHeader(REQUEST_HEADER_USER_ID) Long userId) {
+        log.info("Get item with id = {} belong user with id = {}", itemId, userId);
         return service.getItemById(itemId, userId);
     }
 
     @GetMapping
     public List<ItemDto> getAllItems(@RequestHeader(REQUEST_HEADER_USER_ID) Long userId) {
+        log.info("Get all items of user with id = {}", userId);
         return service.getAllItems(userId);
     }
 
@@ -38,11 +43,13 @@ public class ItemController {
     public ItemDto updateItemById(@RequestBody ItemShortDto itemShortDto,
                                   @PathVariable Long itemId,
                                   @RequestHeader(REQUEST_HEADER_USER_ID) Long userId) {
+        log.info("The item with id = {} of user with id = {} has been updated", itemId, userId);
         return service.updateItemById(itemShortDto, itemId, userId);
     }
 
     @GetMapping("/search")
     public List<ItemDto> searchItemByText(@RequestParam String text) {
+        log.info("Search item by text = \" {} \"", text);
         return service.searchItemByText(text);
     }
 
@@ -50,7 +57,7 @@ public class ItemController {
     @PostMapping("/{itemId}/comment")
     public CommentDto addComment(@PathVariable Long itemId,
                                  @RequestHeader(REQUEST_HEADER_USER_ID) Long userId,
-                                 @Valid @RequestBody CommentDto commentDto) {
+                                 @RequestBody CommentDto commentDto) {
         return service.addComment(commentDto, userId, itemId);
     }
 }
